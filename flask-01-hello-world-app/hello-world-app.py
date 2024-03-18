12b4fb0a82de69f22a36c053d735a4c0273101db
Bozz <boz30can43@gmail.com>
2024-03-18 20:36:31 +0100

    second push

--- flask-01-hello-world-app/hello-world-app.py
+++ flask-01-hello-world-app/hello-world-app.py
@@ -1,12 +1,23 @@
 from flask import Flask 
 app = Flask(__name__)
 
+@app.route("/")
+def head():
+    return "hello world"
 
+@app.route("/second")
+def head2():
+    return "second page"
 
+@app.route("/thirdt")
+def head3():
+    return "this is thirdt page"
 
-
+@app.route("/forth/<string:id>")
+def forth(id):
+    return f"ID of this page is {id}"
 
 if __name__ == '__main__':
 
-    app.run(debug=True, port=30000)
-    # app.run(host= '0.0.0.0', port=8081)
+#    app.run(debug=True, port=30000)
+ app.run(host= '0.0.0.0', port=8081)
